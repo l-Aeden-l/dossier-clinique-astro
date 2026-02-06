6e3e34f8b08a2d17b6482cdfc4373b8e34f6ac93
l-Aeden-l <hauguel.frederic@gmail.com>
2026-02-07 00:47:00 +0100

    Initial commit – ajout du projet Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { z } from 'astro/zod';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  collections: {
+    parcours: {
+      type: 'content',
+      schema: ({ z }) => z.object({
+        title: z.string(),
+        period: z.string(),
+        content: z.string(),
+        date: z.date()
+      })
+    }
+  }
+});
